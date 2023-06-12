57985cfc9a3a930f2c183c19aaba79ab229b25f4
ErwinKomen <E.Komen@ru.nl>
2023-06-12 15:53:17 +0200

    double check 'predictor' processing

--- labdas/multil_rcalc/assets/functions.R
+++ labdas/multil_rcalc/assets/functions.R
@@ -98,51 +98,84 @@ forestPrepare <- function(dataset, filtervar="", predictor="", useDataFilter=FAL
     
   } else {
     # A predictor is provided: using model 2
+
+    # Double check: filtervar may not equal predictor
+    if (filtervar == predictor) {
+
+        # Should generate an error message
+        short_summary <- "ERROR"
+        extended_summary <- "The [predictor] may not be equal to the [filtervar]"
+
+    } else {
     
-    # Filter eerst op alleen de positieve effect sizes en bereken dan opnieuw de covariance matrices:
+        # Filter eerst op alleen de positieve effect sizes en bereken dan opnieuw de covariance matrices:
     
-    dat2 <- dat %>% filter(g_correct_sign >= 0)
+        dat2 <- dat %>% filter(g_correct_sign >= 0)
     
-    # create variance-covariance matrices:
-    V0b <- vcalc(g_var_avg, cluster = sample, type = outcome, grp1 = monolingual_group, grp2 = bilingual_group, w1 = n_L1, w2 = n_2L1, rho = 0, data = dat2)
-    V0.6b <- vcalc(g_var_avg, cluster = sample, type = outcome, grp1 = monolingual_group, grp2 = bilingual_group, w1 = n_L1, w2 = n_2L1, rho = 0.6, data = dat2)
-    V0.95b <- vcalc(g_var_avg, cluster = sample, type = outcome, grp1 = monolingual_group, grp2 = bilingual_group, w1 = n_L1, w2 = n_2L1, rho = 0.95, data = dat2)  
+        # create variance-covariance matrices:
+        V0b <- vcalc(g_var_avg, cluster = sample, type = outcome, grp1 = monolingual_group, grp2 = bilingual_group, w1 = n_L1, w2 = n_2L1, rho = 0, data = dat2)
+        V0.6b <- vcalc(g_var_avg, cluster = sample, type = outcome, grp1 = monolingual_group, grp2 = bilingual_group, w1 = n_L1, w2 = n_2L1, rho = 0.6, data = dat2)
+        V0.95b <- vcalc(g_var_avg, cluster = sample, type = outcome, grp1 = monolingual_group, grp2 = bilingual_group, w1 = n_L1, w2 = n_2L1, rho = 0.95, data = dat2)  
   
-    # Filter eerst op alleen de positieve effect sizes en bereken dan opnieuw de covariance matrices:
+        # Hierna kunnen de modellen gerund worden, waarbij “predictor” de predictor is die de gebruiker geselecteerd heeft:
+        meta2a <- rma.mv(g_correct_sign, V0.6b, data = dat2, mods = ~predictor -1, random = ~ 1|research_group/data_collection/observation)
+        model2a <- conf_int(meta2a, vcov = "CR2")
+    
+        meta2b <- rma.mv(g_correct_sign, V0b, data = dat2, mods = ~predictor -1, random = ~ 1|research_group/data_collection/observation)
+        model2b <- conf_int(meta2b, vcov = "CR2")
+    
+        meta2c <- rma.mv(g_correct_sign, V0.95b, data = dat2, mods = ~predictor -1, random = ~ 1|research_group/data_collection/observation)
+        model2c <- conf_int(meta2c, vcov = "CR2")
+    
+        # LET OP: als de gebruiker target_or_child_system heeft gekozen als variabele, 
+        #   wordt naast target_or_child_system ook de predictor surface_overlap_author toegevoegd 
+        #   én de interactie tussen deze twee variabelen:
+        if (filtervar == "target_or_child") {
+          meta2a <- rma.mv(g_correct_sign, V0.6b, data = dat2, mods =~target_or_child_system*surface_overlap_author -1, random = ~ 1|research_group/data_collection/observation)
+          ## Warning: Rows with NAs omitted from model fitting.
+          model2a <- conf_int(meta2a, vcov = "CR2")
       
-    dat2 <- dat %>% filter(g_correct_sign >= 0)
-    
-    # create variance-covariance matrices:
-    V0b <- vcalc(g_var_avg, cluster = sample, type = outcome, grp1 = monolingual_group, grp2 = bilingual_group, w1 = n_L1, w2 = n_2L1, rho = 0, data = dat2)
-    V0.6b <- vcalc(g_var_avg, cluster = sample, type = outcome, grp1 = monolingual_group, grp2 = bilingual_group, w1 = n_L1, w2 = n_2L1, rho = 0.6, data = dat2)
-    V0.95b <- vcalc(g_var_avg, cluster = sample, type = outcome, grp1 = monolingual_group, grp2 = bilingual_group, w1 = n_L1, w2 = n_2L1, rho = 0.95, data = dat2)
-    
-    # Hierna kunnen de modellen gerund worden, waarbij “predictor” de predictor is die de gebruiker geselecteerd heeft:
-    meta2a <- rma.mv(g_correct_sign, V0.6b, data = dat2, mods = ~predictor -1, random = ~ 1|research_group/data_collection/observation)
-    model2a <- conf_int(meta2a, vcov = "CR2")
-    
-    meta2b <- rma.mv(g_correct_sign, V0b, data = dat2, mods = ~predictor -1, random = ~ 1|research_group/data_collection/observation)
-    model2b <- conf_int(meta2b, vcov = "CR2")
-    
-    meta2c <- rma.mv(g_correct_sign, V0.95b, data = dat2, mods = ~predictor -1, random = ~ 1|research_group/data_collection/observation)
-    model2c <- conf_int(meta2c, vcov = "CR2")
-    
-    # LET OP: als de gebruiker target_or_child_system heeft gekozen als variabele, 
-    #   wordt naast target_or_child_system ook de predictor surface_overlap_author toegevoegd 
-    #   én de interactie tussen deze twee variabelen:
-    if (filtervar == "target_or_child") {
-      meta2a <- rma.mv(g_correct_sign, V0.6b, data = dat2, mods =~target_or_child_system*surface_overlap_author -1, random = ~ 1|research_group/data_collection/observation)
-      ## Warning: Rows with NAs omitted from model fitting.
-      model2a <- conf_int(meta2a, vcov = "CR2")
+          meta2b <- rma.mv(g_correct_sign, V0b, data = dat2, mods = ~target_or_child_system*surface_overlap_author -1, random = ~ 1|research_group/data_collection/observation)
+          ## Warning: Rows with NAs omitted from model fitting.
+          model2b <- conf_int(meta2b, vcov = "CR2")
       
-      meta2b <- rma.mv(g_correct_sign, V0b, data = dat2, mods = ~target_or_child_system*surface_overlap_author -1, random = ~ 1|research_group/data_collection/observation)
-      ## Warning: Rows with NAs omitted from model fitting.
-      model2b <- conf_int(meta2b, vcov = "CR2")
+          meta2c <- rma.mv(g_correct_sign, V0.95b, data = dat2, mods = ~target_or_child_system*surface_overlap_author -1, random = ~ 1|research_group/data_collection/observation)
+          ## Warning: Rows with NAs omitted from model fitting.
+          model2c <- conf_int(meta2c, vcov = "CR2")
       
-      meta2c <- rma.mv(g_correct_sign, V0.95b, data = dat2, mods = ~target_or_child_system*surface_overlap_author -1, random = ~ 1|research_group/data_collection/observation)
-      ## Warning: Rows with NAs omitted from model fitting.
-      model2c <- conf_int(meta2c, vcov = "CR2")
-      
+        }
+    
+        # Hele korte samenvatting van de analyse gebaseerd op het model met predictors (model 2): 
+        #  Number of studies, number of datapoints, effect size, confidence interval. 
+        #
+        # Create a 'short summary'
+        short_summary <- list(
+          # Number of studies
+          number_studies = length(unique(dat2$short_cite)),
+          # Number of datapoints
+          number_data = nrow(dat2),
+          # effect size
+          effect_size = model2a$beta,
+          # lower bound confidence interval
+          conf_lbound = model2a$CI_L,
+          # upper bound confidence interval
+          conf_ubound = model2a$CI_U
+        )
+    
+        # Uitgebreide samenvatting van model 2 als de gebruiker wel predictors geselecteerd heeft
+        extended_summary <- list(
+          # The model call - but as a string
+          model_call = capture.output(meta2a$call),
+          # de informatie die hierin staat t/m Test for Heterogeneity - as a string
+          summary = capture.output(summary(meta2a)),
+          # de informatie die hierin staat, achter Estimate én achter SE moet (r = 0.60) komen te staan
+          model2a = mode21a,
+          # Hieruit de estimates en SEs met "(r = 0.0)" erachter
+          model2b = model2b,
+          # Hieruit de estimates en SEs met "(r = 0.95)" erachter
+          model12c = model2c
+        )
+
     }
     
   }
